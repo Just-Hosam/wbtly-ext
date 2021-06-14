174d15cfc4cee2baf4fbb9bbed768deb587f661b
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-06-13 21:07:29 -0600

    App component and styling

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Header from '../Header/Header';
+import UrlsList from '../UrlsList/UrlsList';
+
+function App() {
+	return (
+		<div className="App">
+			<Header />
+			<UrlsList />
+		</div>
+	);
+}
+
+export default App;
