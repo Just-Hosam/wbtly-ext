0edbd7f2db1e2e1dab3423869d4b426d6a479acb
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-06-14 09:14:00 -0600

    Connect UrlsList to API

--- src/components/UrlsList/UrlsList.tsx
+++ src/components/UrlsList/UrlsList.tsx
@@ -1,5 +1,9 @@
-import './UrlsList.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import UrlsListItem from './UrlsListItem/UrlsListItem';
+
+import './UrlsList.css';
 
 interface Url {
 	id: number;
@@ -9,22 +13,20 @@ interface Url {
 	description: string;
 }
 
-const data = {
-	id: 1,
-	user_id: 1,
-	short_url: 'shortURL',
-	long_url: 'longURL',
-	description: 'description',
-};
-
 const UrlsList = () => {
-	return (
-		<ul>
-			<UrlsListItem data={data} />
-			<UrlsListItem data={data} />
-			<UrlsListItem data={data} />
-		</ul>
-	);
+	const userId = 3;
+	const [urlsData, setUrlsData] = useState<Url[]>([]);
+
+	useEffect(() => {
+		axios
+			.get(`http://www.wbtly.ca/users/${userId}/urls`)
+			.then((res) => setUrlsData(res.data))
+			.catch((err: Error) => console.log('Error at useEffect in UrlsList', err));
+	}, [userId]);
+
+	const UrlsComponents = urlsData.map((url, urlIdx) => <UrlsListItem key={urlIdx} data={url} />);
+
+	return <ul>{UrlsComponents}</ul>;
 };
 
 export default UrlsList;
